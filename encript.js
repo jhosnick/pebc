121e10edbc51d430d0f3fab056b6f46f47328538
jhosnick <86758311+jhosnick@users.noreply.github.com>
2021-06-30 23:51:48 -0400

    Add files via upload

---
+++ encript.js
@@ -0,0 +1,28 @@
+var prodId = getParameterByName('prodId');
+
+const inventario = [
+  /*{ nombre: 'Rodrigo Salazar', ci: 444888 },
+ */
+  { nro:666, nombre: 'Brian Rodrigo Salazar Quispe', ci: 8468662 ,cargo:'presidente' , macro: 'la paz', dist: '666'},
+
+  { nro:1, nombre: 'Josue Viktor Morales', ci: 6957485 ,cargo:'Coordinador Político D-15 Coordinador Político D-15 Coordinador Político D-15' , macro: 'San Antonio', dist: '15'},
+
+];
+var item = inventario.find(item => item.nro === parseInt(prodId, 10));
+var item = inventario.find(item => item.ci === parseInt(prodId, 10));
+
+function getParameterByName(name) {
+  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+    results = regex.exec(location.search);
+  return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+//console.log(item.nombre);
+function getMiembro() {
+  document.getElementById("nro").innerHTML = item.nro;
+  document.getElementById("nombres").innerHTML = item.nombre;
+  document.getElementById("ci").innerHTML = item.ci;
+  document.getElementById("cargo").innerHTML = item.cargo;
+  document.getElementById("macro").innerHTML = item.macro;
+  document.getElementById("dist").innerHTML = item.dist;
+}
